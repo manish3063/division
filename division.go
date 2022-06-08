02c14e4d39a38fcc846f104be21b9b46f2995cde
manish3063 <manishsingh111095@gmail.com>
2022-06-08 16:58:09 +0530

    division

---
+++ division.go
@@ -0,0 +1,12 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	num1 := 20
+	num2 := 10
+	result := num1 / num2
+	fmt.Println("result is: ", result)
+}
